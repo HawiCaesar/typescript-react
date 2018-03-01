53a7de07b0faf7cfd25878e22d4a0a318dc1b499
HawiCaesar <brian.odhiambo932@gmail.com>
2018-03-01 16:40:05 +0300

    Initial Commit

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { FirstComponent } from './src/components/FirstComponent';
+
+ReactDOM.render(
+    <div>
+        <FirstComponent />
+    </div>,
+    document.getElementById('root')
+);
