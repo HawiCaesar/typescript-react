32b08d06c062363f6e6a3da08f4bfc2fdfdb3c02
HawiCaesar <brian.odhiambo932@gmail.com>
2018-04-18 18:41:42 +0300

    chore(front-page): clean up for intro page

--- src/components/FirstComponent.tsx
+++ src/components/FirstComponent.tsx
@@ -7,14 +7,17 @@ export class FirstComponent extends React.Component <{}> {
     return (
         <div>
             {/* React components must have a wrapper node/element */}
-            <h1>A Simple React Component Example with Typescript</h1>
+            <h1 style={{textAlign: 'center'}}>
+              A Simple React Component Example with Typescript
+            </h1>
             <div>
-                <img width={1200} height={600} src={Logo} />
+                <img
+                    style={{marginLeft: '8%'}}
+                    width={1200}
+                    height={600}
+                    src={Logo}
+                />
             </div>
-            <p>
-              This is a old and new component which shows the logrocket
-              logo. For more info on Logrocket, please visit Https://logrocket.com
-            </p>
         </div>
     );
   }
